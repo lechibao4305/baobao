87ba744f9a5dc50d5221e52861611e505e656c6c
lechibao4305 <44374582+lechibao4305@users.noreply.github.com>
2019-12-13 02:26:10 +0700

    Update Admin.js

--- src/Component/Admin/Admin.js
+++ src/Component/Admin/Admin.js
@@ -1,42 +1,40 @@
-import React, { Component } from 'react';
-import Title from './Title'
-import Search from './Search'
-import Sort from './Sort'
-import Form from './Form'
-import ListItem from './ListItem'
-import SweetAlert from 'sweetalert-react'
-import Table from './Table';
-import SideNav from './SideNav';
-import { Redirect, Router } from 'react-router-dom'
-import RouterAdmin from './RouterAdmin';
+import React, { Component } from "react";
+import Title from "./Title";
+import Search from "./Search";
+import Sort from "./Sort";
+import Form from "./Form";
+import ListItem from "./ListItem";
+import SweetAlert from "sweetalert-react";
+import Table from "./Table";
+import SideNav from "./SideNav";
+import { Redirect, Router } from "react-router-dom";
+import RouterAdmin from "./RouterAdmin";
 class Admin extends Component {
-    constructor(props) {
-        super(props);
-
-        localStorage.removeItem("token");
-        // const token = localStorage.getItem("token")
-        // let check = true
-        // if (token == null) {
-        //     check = false;
-        // }
-        let check
-        this.state = {
-            check
-        }
+  constructor(props) {
+    super(props);
+    const token = localStorage.getItem("token");
+    let check = true;
+    if (token == null) {
+      check = false;
     }
-    render() {
-        if (this.state.check == false) {
-            return <Redirect to="/ErrorPage" />
-        }
-        return (
-            <React.Fragment>
-                <SideNav />
-                <div className="flex-wrap">
-                    <RouterAdmin />
-                </div>
-            </React.Fragment>
-        );
+    this.state = {
+      check
+    };
+  }
+ 
+  render() {
+    if (this.state.check == false) {
+      return <Redirect to="/Login" />;
     }
+    return (
+      <React.Fragment>
+        <SideNav />
+        <div className="flex-wrap">
+          <RouterAdmin />
+        </div>
+      </React.Fragment>
+    );
+  }
 }
 
 export default Admin;
